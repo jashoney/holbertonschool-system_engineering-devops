dee8319be6395709a016250ad318af327d6619db
jashoney <4479@holbertonschool.com>
2022-09-13 12:02:16 +1000

    changed task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""" uses jsonplaceholder REST api to return info
-    all tasks owned by an employee are saved to csv
-"""
+""" uses jsonplaceholder REST api to return info 
+    all tasks owned by an employee are saved to csv format """
 
 import csv
-import json
-import requests
+from requests import get
 from sys import argv
 
 if __name__ == "__main__":
     id = argv[1]
+
     user_url = "https://jsonplaceholder.typicode.com/users/"
     to_do_url = "https://jsonplaceholder.typicode.com/todos/?userId="
-    user = requests.get(user_url + id).json()
-    to_dos = requests.get(to_do_url + id).json()
-    filename = f"{id}.csv"
-    with open(filename, 'w', newline="") as export_file:
-        tasks = csv.writer(export_file, quoting=csv.QUOTE_ALL)
+    user = get(user_url + id).json()
+    to_dos = get(to_do_url + id).json()
+
+    filename = id + ".csv"
+
+    with open(filename, mode='w') as export_file:
+        writer = csv.writer(export_file, quoting=csv.QUOTE_ALL)
         for task in to_dos:
-            tasks.writerow([int(id),
-                            user.get("username"),
-                            task.get("completed"),
-                            task.get("title")])
+            writer.writerow([task["userId"], user["username"],
+                            task["completed"], task["title"]])
