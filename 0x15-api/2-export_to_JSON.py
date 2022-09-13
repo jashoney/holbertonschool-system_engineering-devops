723ef6a05df21fd55e1f28181b0ca2dd1777101c
jashoney <4479@holbertonschool.com>
2022-09-13 12:13:51 +1000

    added task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-""" uses jsonplaceholder REST api to return info
-    exports data to json file
-"""
+""" exports data to a json file """
 
 import json
-import requests
+from requests import get
 from sys import argv
 
 if __name__ == "__main__":
     id = argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/"
     to_do_url = "https://jsonplaceholder.typicode.com/todos/?userId="
-    user = requests.get(user_url + id).json()
-    to_dos = requests.get(to_do_url + id).json()
-    name = user.get("username")
-    tasks = []
+    user = get(user_url + id).json()
+    to_dos = get(to_do_url + id).json()
+
+    tasks_to_do = []
     for task in to_dos:
         task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = name
-        tasks.append(task_dict)
-    json_obj = {}
-    json_obj[id] = tasks
-    filename = f"{id}.json"
-    with open(filename, 'w') as jsonfile:
-        json.dump(json_obj, jsonfile)
+        task_dict["task"] = task["title"]
+        task_dict["completed"] = task['completed']
+        task_dict["username"] = user["username"]
+        tasks_to_do.append(task_dict)
+
+    user_to_do_dict = {}
+    user_to_do_dict[id] = tasks_to_do
+
+    filename = id + ".json"
+    with open(filename, 'w') as json_file:
+        json.dump(user_to_do_dict, json_file)
