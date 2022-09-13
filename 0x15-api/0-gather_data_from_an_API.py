fd56cb8e07defd6a2917238056e1e1862cd88d55
jashoney <4479@holbertonschool.com>
2022-09-13 11:16:13 +1000

    removed f-strings

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" uses jsonplaceholder REST api to return info """
+""" uses jsonplaceholder REST api to return info 
+    program accepts an interger as a parameter
+    identifying the employee
+"""
 
 import json
 import requests
@@ -7,15 +10,20 @@ from sys import argv
 
 if __name__ == "__main__":
     id = argv[1]
+
     user_url = "https://jsonplaceholder.typicode.com/users/"
     to_do_url = "https://jsonplaceholder.typicode.com/todos/?userId="
     user = requests.get(user_url + id).json()
     to_dos = requests.get(to_do_url + id).json()
+
     tasks_done = []
     for task in to_dos:
         if task.get("completed") is True:
             tasks_done.append(task.get("title"))
+
     name = user.get('name')
-    tasks_string = f"({len(tasks_done)}/{len(to_dos)}):"
-    print(f"Employee {name} is done with tasks {tasks_string}")
-    print("\n".join(f"\t {task}" for task in tasks_done))
+
+    print("Employee {} is done with tasks ({}/{}:".
+          format(name, len(tasks_done), len(to_dos)))
+    for task in tasks_done:
+        print("\t {}".format(task))
